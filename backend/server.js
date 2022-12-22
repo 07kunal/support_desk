c173c4e96fad2f606af6a113b9d7eb0f757aa560
kunal <kunalgautam200@gmail.com>
2022-12-22 09:11:39 +0530

    updated deploy

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
+const path = require('path')
 const express = require('express');
+
 const dotenv = require('dotenv').config();
 const colors = require('colors')
 const { errorHandler } = require("./middleware/errorMiddleware")
@@ -14,19 +16,35 @@ const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 
-// method GET 
-app.get('/', (req, res) => {
-    // res.send('Hello')
-    // sending the data in json format
-    // res.status(200).json({message:'welcome to kunal support desk api'})
-    res.json({ message: 'welcome to Kunal Support Desk API' })
+// // method GET 
+// app.get('/', (req, res) => {
+//     // res.send('Hello')
+//     // sending the data in json format
+//     // res.status(200).json({message:'welcome to kunal support desk api'})
+//     res.json({ message: 'welcome to Kunal Support Desk API' })
 
-})
+// })
 // adding the another folder file in this way . Routes, here app is connected to the route file so it's working for the api urls
 
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
 
+// serve Frontend
+if (process.env.NODE_ENV === 'production') {
+    // set build folder as static
+    app.use(express.static(path.join(__dirname, "../frontend/build")))
+
+    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontent', 'build', 'index.html'))
+} else {
+    // method GET 
+    app.get('/', (req, res) => {
+        // res.send('Hello')
+        // sending the data in json format
+        // res.status(200).json({message:'welcome to kunal support desk api'})
+        res.json({ message: 'welcome to Kunal Support Desk API' })
+
+    })
+}
 
 app.use(errorHandler)
 
